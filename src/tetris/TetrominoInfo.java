6766cbbebb2c042fb684f17079a1d5fedbabb9b8
Daniel <vdanielrolandi@gmail.com>
2013-11-23 20:08:21 -0800

    Wrote precomputed table for rotation points.

--- src/tetris/TetrominoInfo.java
+++ src/tetris/TetrominoInfo.java
@@ -18,10 +18,71 @@ public enum TetrominoInfo {
   private static final Color Tcolor = new Color(255, 0, 255);
   private static final Color Lcolor = new Color(255, 180, 0);
   private static final Color Jcolor = new Color(0, 180, 255);
-  
-  private static final Point point32 = new Point(3, 2);
-  private static final Point point33 = new Point(3, 3);
+    
   private static final Point point44 = new Point(4, 4);
+  private static final Point[][] allPoints;  
+  static{
+    Point[][] allPointsTemp = new Point[4][4];
+    for(int x = 0; x < 4; x++){
+      for(int y = 0; y < 4; y++){
+        allPointsTemp[x][y] = new Point(x, y);
+      }
+    }
+    allPoints = allPointsTemp;
+  }
+  
+  // NOTE: Another alternative is using rotation for each time
+  // but precomputed values uses less computation.
+  // row = state, col = blockID
+  
+  private static final Point[][] pointsI = {
+    {allPoints[0][1], allPoints[1][1], allPoints[2][1], allPoints[3][1]},
+    {allPoints[2][0], allPoints[2][1], allPoints[2][2], allPoints[2][3]},
+    {allPoints[3][2], allPoints[2][2], allPoints[1][2], allPoints[0][2]},
+    {allPoints[1][3], allPoints[1][2], allPoints[1][1], allPoints[1][0]}
+  };
+  
+  private static final Point[][] pointsZ = {
+    {allPoints[0][1], allPoints[1][1], allPoints[1][2], allPoints[2][2]},
+    {allPoints[1][0], allPoints[1][1], allPoints[0][1], allPoints[0][2]},
+    {allPoints[2][1], allPoints[1][1], allPoints[1][0], allPoints[0][0]},
+    {allPoints[1][2], allPoints[1][1], allPoints[2][1], allPoints[2][0]}
+  };
+  
+  private static final Point[][] pointsS = {
+    {allPoints[2][1], allPoints[1][1], allPoints[1][2], allPoints[0][2]},
+    {allPoints[1][2], allPoints[1][1], allPoints[0][1], allPoints[0][0]},
+    {allPoints[0][1], allPoints[1][1], allPoints[1][0], allPoints[2][0]},
+    {allPoints[1][0], allPoints[1][1], allPoints[2][1], allPoints[2][2]}
+  };
+  
+  private static final Point[][] pointsO = {
+    {allPoints[0][0], allPoints[1][0], allPoints[0][1], allPoints[1][1]},
+    {allPoints[0][0], allPoints[1][0], allPoints[0][1], allPoints[1][1]},
+    {allPoints[0][0], allPoints[1][0], allPoints[0][1], allPoints[1][1]},
+    {allPoints[0][0], allPoints[1][0], allPoints[0][1], allPoints[1][1]}
+  };
+  
+  private static final Point[][] pointsT = {
+    {allPoints[0][1], allPoints[1][1], allPoints[1][0], allPoints[2][1]},
+    {allPoints[1][0], allPoints[1][1], allPoints[2][1], allPoints[1][2]},
+    {allPoints[2][1], allPoints[1][1], allPoints[1][2], allPoints[0][1]},
+    {allPoints[1][2], allPoints[1][1], allPoints[0][1], allPoints[1][0]}
+  };
+  
+  private static final Point[][] pointsL = {
+    {allPoints[0][1], allPoints[1][1], allPoints[2][1], allPoints[2][0]},
+    {allPoints[1][0], allPoints[1][1], allPoints[1][2], allPoints[2][2]},
+    {allPoints[2][1], allPoints[1][1], allPoints[0][1], allPoints[0][2]},
+    {allPoints[1][2], allPoints[1][1], allPoints[1][0], allPoints[0][0]}
+  };
+  
+  private static final Point[][] pointsJ = {
+    {allPoints[0][1], allPoints[1][1], allPoints[2][1], allPoints[0][0]},
+    {allPoints[1][0], allPoints[1][1], allPoints[1][2], allPoints[2][0]},
+    {allPoints[2][1], allPoints[1][1], allPoints[0][1], allPoints[2][2]},
+    {allPoints[1][2], allPoints[1][1], allPoints[1][0], allPoints[0][2]}
+  };
   
   /**
    * Returns random Tetromino type.
@@ -60,12 +121,12 @@ public enum TetrominoInfo {
    */
   public static Point getSpawnPoint(TetrominoInfo type){
     switch(type){
-      case I: return point32;
+      case I: return allPoints[3][2];
       case Z:
       case S:
       case T:
       case L:
-      case J: return point33;
+      case J: return allPoints[3][3];
       case O: return point44;
       default: throw new IllegalArgumentException();
     }
